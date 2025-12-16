930f6d7c825c5ad847d6ec0d49b60236c7280034
Kutuy <febry.gundar@gmail.com>
2025-12-16 11:56:14 +0700

    Update kontol11.lua

--- kontol11.lua
+++ kontol11.lua
@@ -15,6 +15,8 @@ local Lighting = game:GetService("Lighting")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local ItemsFolder = Workspace:WaitForChild("Items")
 
 local Camera = Workspace.CurrentCamera
 ---------------------------------------------------------
